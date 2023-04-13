8635e925ec35c9f3b2172890b6c16cd25879831f
MostlyRice <amazinzygy@gmail.com>
2023-04-13 14:19:21 -0500

    Added button that went to the homepage

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request  # NOT the same as requests
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def login():
     return render_template('index.html')
 
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run()
