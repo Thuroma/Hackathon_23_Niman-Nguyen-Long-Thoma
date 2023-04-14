20c676ddcfc8781939d24d209924e5dc6f500ff2
RageKage <99547837+RageKage@users.noreply.github.com>
2023-04-14 08:50:57 -0500

    new login css

--- apis/moneyMovement.py
+++ apis/moneyMovement.py
@@ -9,6 +9,33 @@ load_dotenv()
 moneyMovementAPI = os.getenv('MONEY_MOVEMENT_API_KEY')
 if not apiKey:
     raise ValueError('MONEY_MOVEMENT_API_KEY not found in environment variables')
+else: 
+    import requests
+    
+    API_URL = "https://alpha-api.usbank.com/innovation/bank-node/customer-accounts/v1/"
+    API_KEY = "nPtN8LARVh1Q4b0GZKuFnsMATIc0TJet"
+    API_SECRET = "20LxAZoBaJZjMkAI"
+    
+    def find_customer(customer_id):
+        auth = (API_KEY, API_SECRET)
+        
+        headers = {
+        "content-type": "application/json",
+        "Accept": "application/json",
+        }
+        
+        url = API_URL + "customer/" + customer_id + "/accounts"
+        
+        try:
+            response = requests.get(url, auth=auth, headers=headers)
+        
+            print(response.status_code)
+            print(response.json())
+        except Exception as e:
+            print(e)
+    
+    customer_id = "6763895841"
+    find_customer(customer_id)
 
 moneyMovementSecret = os.getenv('MONEY_MOVEMENT_API_SECRET')
 if not apiKey:
