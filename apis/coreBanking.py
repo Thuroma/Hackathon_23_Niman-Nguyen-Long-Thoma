650d058a94ebc1138f8c8e008a878ab003995a4c
MostlyRice <amazinzygy@gmail.com>
2023-04-13 20:02:15 -0500

    edited env, added req.txt

--- apis/coreBanking.py
+++ apis/coreBanking.py
@@ -1,21 +1,25 @@
 import requests
 import os
 from pprint import pprint
+from dotenv import load_dotenv
+
+# Load the environment variables from .env file
+load_dotenv()
+
+# Get the API keys from the environment variables
+API_KEY = os.getenv('US_BANKING_API_KEY')
+if not API_KEY:
+    raise ValueError('US_BANKING_API_KEY not found in environment variables')
+
+API_SECRET = os.getenv('US_BANKING_SECRET_KEY')
+if not API_SECRET:
+    raise ValueError('US_BANKING_SECRET_KEY not found in environment variables')
 
 def get_customer_account_info():
 
     try:
         API_URL = "https://alpha-api.usbank.com/innovation/bank-node/customer-accounts/v1/"
-        API_KEY = os.environ.get('US_BANKING_API_KEY')
-        API_SECRET = os.environ.get('US_BANKING_SECRET_KEY')
-
-        if not API_KEY:
-            raise ValueError('US_BANKING_API_KEY not found in environment variables')
-        if not API_SECRET:
-            raise ValueError('US_BANKING_API_SECRET not found in environment variables')
-
         customer_id = "6763895841"
-
         auth = (API_KEY, API_SECRET)
 
         headers = {
@@ -38,16 +42,7 @@ def get_customer_personal_info():
 
     try:
         API_URL = "https://alpha-api.usbank.com/innovation/bank-node/customer-accounts/v1/"
-        API_KEY = os.environ.get('US_BANKING_API_KEY')
-        API_SECRET = os.environ.get('US_BANKING_SECRET_KEY')
-
-        if not API_KEY:
-            raise ValueError('US_BANKING_API_KEY not found in environment variables')
-        if not API_SECRET:
-            raise ValueError('US_BANKING_API_SECRET not found in environment variables')
-
         customer_id = "6763895841"
-
         auth = (API_KEY, API_SECRET)
 
         headers = {
